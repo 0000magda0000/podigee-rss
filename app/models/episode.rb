d267fd8f90426af80188759f94cdeb0eed1548ff
Magda Barthofer <magda.barthofer@gmail.com>
2021-02-18 18:17:01 +0100

    added validations to urls

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,5 @@
 class Episode < ApplicationRecord
   belongs_to :podcast
+  URL_REGEXP = /\A^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  validates :audio_file_url, format: { with: URL_REGEXP, message: 'You provided invalid URL' }
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,3 +1,8 @@
 class Podcast < ApplicationRecord
   has_many :episodes
+  URL_REGEXP = /\A^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  validates :image_url, format: { with: URL_REGEXP, message: 'You provided invalid URL' }
+  validates :website_url, format: { with: URL_REGEXP, message: 'You provided invalid URL' }
+  validates :feed_url, format: { with: URL_REGEXP, message: 'You provided invalid URL' }
+
 end
