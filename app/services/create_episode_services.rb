8b48f92379e2098d32f94466f297ee566a9383c7
Magda Barthofer <magda.barthofer@gmail.com>
2021-02-19 10:27:57 +0100

    refactored services

--- app/services/create_episode_services.rb
+++ app/services/create_episode_services.rb
@@ -2,44 +2,35 @@ require 'httparty'
 
 class CreateEpisodeServices
 
-  def initialize(url)
-    @url = url
-  end
-
   def parse_xml(url)
     xml = HTTParty.get(url).body
-    feed = Feedjira.parse(xml)
-    # what comes after entries or after feed? check parsed xml
+    @feed = Feedjira.parse(xml)
+  end
 
-    feed.entries.each do |entry|
-      @title = entry.title
-      @number = entry.url[/\d+/].to_i
-      @description = entry.summary
-      @audio_duration = entry.itunes_duration
-      @audio_file_url = entry.url
-      if Podcast.where("website_url LIKE ?", "%#{@audio_file_url}%").first.nil?
-        @podcast_id = Podcast.first.id
-      else
-        @podcast_id = Podcast.where("website_url LIKE ?", "%#{@audio_file_url}%").first.id
-      end
-
-      @episodes_array = []
-      @episodes_array << Episode.new(
-        title: @title,
-        number: @number,
-        description: @description,
-        audio_duration: @audio_duration,
-        audio_file_url: @audio_file_url.downcase,
-        podcast_id: @podcast_id
+  def create_episodes(parsed_xml)
+    @feed.entries.each do |entry|
+      # I don't know how to nicely get the responding podcast_id
+      podcast_id = if Podcast.where("website_url LIKE ?",
+                                      "%#{entry.url}%").first.nil?
+                      Podcast.first.id
+                    else
+                      Podcast.where("website_url LIKE ?",
+                                      "%#{entry.url.downcase}%").first.id
+                    end
+      Episode.create!(
+        title: entry.title,
+        number: entry.url[/\d+/].to_i,
+        description: entry.summary,
+        audio_duration: entry.itunes_duration,
+        audio_file_url: entry.url.downcase,
+        podcast_id: podcast_id
       )
-
-      @episodes_array.each do |episode|
-        episode.save!
-      end
     end
   end
 end
 
+# in Rails Console:
 # URL = "https://theaussieenglishpodcast.podigee.io/feed/mp3"
-# episode = CreateEpisodeService.new(URL)
-# episode.parse_xml(URL)
+# episodes = CreateEpisodeServices.new
+# parsed_xml = episodes.parse_xml(URL)
+# episodes.create_episodes(parsed_xml)
--- app/services/create_podcast_services.rb
+++ app/services/create_podcast_services.rb
@@ -1,34 +1,25 @@
 require 'httparty'
 
 class CreatePodcastServices
-  def initialize(url)
-    @url = url
-  end
-
   def parse_xml(url)
     xml = HTTParty.get(url).body
-    feed = Feedjira.parse(xml)
-    # what comes after entries or after feed? check parsed xml
+    @feed = Feedjira.parse(xml)
+  end
 
-    @title = feed.title
-    @description = feed.description
-    @publication_date = feed.last_built.to_date
-    @image_url = feed.image.url.downcase
-    @website_url = feed.url.downcase
-    @feed_url = feed.url.downcase
-
+  def create_podcast(parsed_xml)
     Podcast.create!(
-      title: @title,
-      description: @description,
-      publication_date: @publication_date,
-      image_url: @image_url,
-      website_url: @website_url,
-      feed_url: @feed_url
+      title: @feed.title,
+      description: @feed.description,
+      publication_date: @feed.last_built.to_date,
+      image_url: @feed.image.url.downcase,
+      website_url: @feed.url.downcase,
+      feed_url: @feed.url.downcase
     )
   end
 end
 
+# in `rails console`:
 # URL = "https://theaussieenglishpodcast.podigee.io/feed/mp3"
-# podcast = CreatePodcastService.new(URL)
-# podcast.parse_xml(URL)
-
+# podcast = CreatePodcastServices.new
+# parsed_xml = podcast.parse_xml(URL)
+# podcast.create_podcast(parsed_xml)
